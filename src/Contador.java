73677821bf3dda87448fc35c083f7430b67c8793
LittleDustDevWIN <markraph19@gmail.com>
2024-06-12 18:13:49 -0300

    feat: add exceção

--- src/Contador.java
+++ src/Contador.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Contador {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			//chama o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException exception) {
+            //imprime a mensagem
+			System.err.println("O segundo parâmetro deve ser maior que o primeiro");
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        //valida se parametroUm é MAIOR que parametroDois e lança a exceção
+		if (parametroUm > parametroDois) throw new ParametrosInvalidosException();
+		
+		int contagem = parametroDois - parametroUm;
+        //realiza o for para imprimir os números com base na variável contagem
+		for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+	}
 }
